887b3079563dd0850a100fbce86900a5d0ad7538
BuildTools <unconfigured@null.spigotmc.org>
2023-04-01 00:35:38 -0700

    big changes to user model and user controller. Added json webtoken for login.

--- server/models/User.js
+++ server/models/User.js
@@ -3,10 +3,19 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
+        required: true,
         unique: true,
     },
-    password: String,
-
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must use a valid email address'],
+    },
+    password: {
+        type: String,
+        required: true,
+    },
 }, { timestamps: true } );
 
 const UserModel = mongoose.model('User', userSchema);
